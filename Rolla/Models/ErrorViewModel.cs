c84032ea44202014441ccb56aedce94b7aa48c9e
sahandrbp <124429732+sahandrbp@users.noreply.github.com>
2025-07-17 11:21:06 +0430

    Add project files.

---
+++ Rolla/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rolla.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
