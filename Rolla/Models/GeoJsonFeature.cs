88cc03dcfe7e4d1764bac4cb2210072e523e9588
sahandrbp <124429732+sahandrbp@users.noreply.github.com>
2025-08-07 14:12:50 +0430

    CreatingGeoJsonFeatureViewModel

---
+++ Rolla/Models/GeoJsonFeature.cs
@@ -0,0 +1,17 @@
+﻿namespace Rolla.Models
+{
+    public class GeoJsonFeature
+    {
+        public String Type => "feature";
+
+        public Geometry geometry { get; set; } = new Geometry();
+
+        public Dictionary<string, object> Properties { get; set; }
+    }
+    
+    public class Geometry
+    {
+        public string Type => "Point";
+        public double[] Coordinates { get; set; }
+    }
+}
